aea638430ef688a10ec3689e2f00efe621b7d389
Dominik Bláha <jsem@dominikblaha.cz>
2023-05-21 19:55:18 +0200

    Admin: facade for youtube iframe

--- components/ResponsiveVideo/ResponsiveVideo.tsx
+++ components/ResponsiveVideo/ResponsiveVideo.tsx
@@ -1,6 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react"
-import React from "react"
+import React, { useState } from "react"
+import Image from "next/image"
+import { PROJECT } from "../../projects"
 
 export interface ResponsiveVideoProps {
   src: string
@@ -12,32 +14,67 @@ export const ResponsiveVideo: React.FC<ResponsiveVideoProps> = ({
   src,
   width,
   height,
-}) => (
-  <figure
-    css={css`
-      position: relative;
-      width: 100%;
-      height: 0;
-      padding-top: ${(height / width) * 100}%;
-      margin: 0;
-    `}
-  >
-    <iframe
+}) => {
+  const [isPlayed, setIsPlayed] = useState(false)
+  return (
+    <figure
       css={css`
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
+        position: relative;
         width: 100%;
-        height: 100%;
+        height: 0;
+        padding-top: ${(height / width) * 100}%;
+        margin: 0;
       `}
-      width={width}
-      height={height}
-      src={src}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    />
-  </figure>
-)
+    >
+      {isPlayed ? (
+        <iframe
+          css={css`
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            border: none;
+          `}
+          width={width}
+          height={height}
+          src={
+            isPlayed
+              ? `https://www.youtube.com/embed/${src}?rel=0&showinfo=0&autoplay=1`
+              : ""
+          }
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          loading="lazy"
+        />
+      ) : (
+        <>
+          <Image
+            src={`https://img.youtube.com/vi/${src}/sddefault.jpg`}
+            layout="fill"
+            objectFit="cover"
+            alt="yt thumbnail"
+            priority
+          />
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            css={css`
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+              width: 100px;
+              height: 100px;
+              cursor: pointer;
+            `}
+            onClick={() => setIsPlayed(true)}
+            src={`/${PROJECT}/icons/play.svg`}
+            alt="play"
+          />
+        </>
+      )}
+    </figure>
+  )
+}
