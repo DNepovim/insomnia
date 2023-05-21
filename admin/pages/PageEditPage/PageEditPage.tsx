794421bc5cc80737fcd3353eb1ebaff0e2e0b147
Dominik Bláha <jsem@dominikblaha.cz>
2023-05-21 19:52:38 +0200

    Admin: fix page header on page edit page

--- admin/pages/PageEditPage/PageEditPage.tsx
+++ admin/pages/PageEditPage/PageEditPage.tsx
@@ -1,3 +1,4 @@
+/** @jsxImportSource @emotion/react */
 import { v4 as uuid } from "uuid"
 import * as yup from "yup"
 import SaveOutlined from "@ant-design/icons/lib/icons/SaveOutlined"
@@ -28,6 +29,7 @@ import { User } from "@firebase/auth"
 import { Centered } from "../../components/Centered/Centered"
 import { BlockTemplates } from "../../../blocks/blockTemplates"
 import useSwr from "swr"
+import { css } from "@emotion/react"
 
 export const PageEditPage = ({ user }: { user: User }) => {
   const sensors = useSensors(
@@ -94,99 +96,111 @@ export const PageEditPage = ({ user }: { user: User }) => {
       initialValues={page}
     >
       {(props) => (
-        <PageHeader
-          title={<Typography.Title>{page.title}</Typography.Title>}
-          subTitle={`naposledny upraveno ${props.values.lastEditedTime} uživatelem ${props.values.lastEditedBy}`}
-          breadcrumb={{
-            routes: [
-              { breadcrumbName: "Stránky", path: "" },
-              { breadcrumbName: "Hlavní stránka", path: "" },
-            ],
-          }}
-          extra={[
-            <Button
-              key="preview"
-              icon={<EyeOutlined />}
-              href={`/api/preview?secret=${process.env.NEXT_PUBLIC_PREVIEW_TOKEN}&slug=/`} // TODO dynamic path
-              target="_blank"
-            >
-              Náhled
-            </Button>,
-            <Button
-              key="save"
-              type="primary"
-              icon={<SaveOutlined />}
-              onClick={async () => props.submitForm()}
-              disabled={props.isSubmitting}
-              loading={props.isSubmitting}
-            >
-              Uložit
-            </Button>,
-          ]}
-          footer={
-            <Button
-              icon={<AppstoreAddOutlined />}
-              onClick={() =>
-                props.setFieldValue("blocks", [
-                  ...props.values.blocks,
-                  { id: uuid() },
-                ])
-              }
-            >
-              Přidat blok
-            </Button>
-          }
+        <span
+          css={css`
+            .ant-page-header-heading-left {
+              flex-flow: column;
+              align-items: flex-start;
+            }
+          `}
         >
-          <Form>
-            <DndContext
-              sensors={sensors}
-              collisionDetection={closestCenter}
-              onDragEnd={(event) => {
-                const { active, over } = event
-
-                if (!over || active.id === over.id) {
-                  return
-                }
-
-                const items = props.values.blocks.map((v) => v.id)
-                const overIndex = items.indexOf(over.id)
-                const activeIndex = items.indexOf(active.id)
-                const newOrder = arrayMove(
-                  props.values.blocks,
-                  activeIndex,
-                  overIndex
-                )
-
-                props.setFieldValue("blocks", newOrder)
-              }}
-            >
-              <SortableContext
-                items={props.values.blocks.map((v) => v.id)}
-                strategy={verticalListSortingStrategy}
+          <PageHeader
+            title={<Typography.Title>{page.title}</Typography.Title>}
+            subTitle={`naposledny upraveno ${props.values.lastEditedTime} uživatelem ${props.values.lastEditedBy}`}
+            breadcrumb={{
+              routes: [
+                { breadcrumbName: "Stránky", path: "" },
+                { breadcrumbName: "Hlavní stránka", path: "" },
+              ],
+            }}
+            extra={[
+              <Button
+                key="preview"
+                icon={<EyeOutlined />}
+                href={`/api/preview?secret=${process.env.NEXT_PUBLIC_PREVIEW_TOKEN}&slug=/`} // TODO dynamic path
+                target="_blank"
               >
-                {props.values.blocks.map((block, index) => (
-                  <SortableAdminBlockFields
-                    key={block.id}
-                    index={index}
-                    id={block.id}
-                    {...(block.template
-                      ? blockDefs[block.template as BlockTemplates]
-                      : {})}
-                    onRemove={() =>
-                      props.setFieldValue(
-                        "blocks",
-                        props.values.blocks.filter((_, i) => i !== index)
-                      )
-                    }
-                    onTemplateChange={(template) =>
-                      props.setFieldValue(`blocks[${index}].template`, template)
-                    }
-                  />
-                ))}
-              </SortableContext>
-            </DndContext>
-          </Form>
-        </PageHeader>
+                Náhled
+              </Button>,
+              <Button
+                key="save"
+                type="primary"
+                icon={<SaveOutlined />}
+                onClick={async () => props.submitForm()}
+                disabled={props.isSubmitting}
+                loading={props.isSubmitting}
+              >
+                Uložit
+              </Button>,
+            ]}
+            footer={
+              <Button
+                icon={<AppstoreAddOutlined />}
+                onClick={() =>
+                  props.setFieldValue("blocks", [
+                    ...props.values.blocks,
+                    { id: uuid() },
+                  ])
+                }
+              >
+                Přidat blok
+              </Button>
+            }
+          >
+            <Form>
+              <DndContext
+                sensors={sensors}
+                collisionDetection={closestCenter}
+                onDragEnd={(event) => {
+                  const { active, over } = event
+
+                  if (!over || active.id === over.id) {
+                    return
+                  }
+
+                  const items = props.values.blocks.map((v) => v.id)
+                  const overIndex = items.indexOf(over.id)
+                  const activeIndex = items.indexOf(active.id)
+                  const newOrder = arrayMove(
+                    props.values.blocks,
+                    activeIndex,
+                    overIndex
+                  )
+
+                  props.setFieldValue("blocks", newOrder)
+                }}
+              >
+                <SortableContext
+                  items={props.values.blocks.map((v) => v.id)}
+                  strategy={verticalListSortingStrategy}
+                >
+                  {props.values.blocks.map((block, index) => (
+                    <SortableAdminBlockFields
+                      key={block.id}
+                      index={index}
+                      id={block.id}
+                      {...(block.template
+                        ? blockDefs[block.template as BlockTemplates]
+                        : {})}
+                      onRemove={() =>
+                        props.setFieldValue(
+                          "blocks",
+                          props.values.blocks.filter((_, i) => i !== index)
+                        )
+                      }
+                      onTemplateChange={(template) =>
+                        props.setFieldValue(
+                          `blocks[${index}].template`,
+                          template
+                        )
+                      }
+                    />
+                  ))}
+                </SortableContext>
+              </DndContext>
+            </Form>
+          </PageHeader>
+        </span>
       )}
     </Formik>
   )
