f1390c8f0fa2778310fa434b2073b48ed399f519
Dominik Bláha <jsem@dominikblaha.cz>
2025-01-04 13:51:18 +0100

    udpate branch name

--- tina/config.ts
+++ tina/config.ts
@@ -352,11 +352,7 @@ const videoBlock: Template = {
 
 export const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
-    process.env.HEAD ||
-    "master", // Netlify branch env
+  branch: "main",
   token: process.env.TINA_TOKEN,
   media: {
     // If you wanted cloudinary do this
